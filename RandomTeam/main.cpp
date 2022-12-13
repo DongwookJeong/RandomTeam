fcdd78324a5cc971aaa1ed4394ec8c1053393e8f
JeongDongwook <nolhoon211@gmail.com>
2022-12-13 18:07:53 +0900

    random 만들어보기

--- RandomTeam/main.cpp
+++ RandomTeam/main.cpp
@@ -1,24 +1,67 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 int main() {
-	char str[7] = "김근수";
+	//char str[] = "kimgeunsoo";
+	//char str[] = "parkjongin";
+	//char str[] = "mageunwon";
+	//char str[] = "parkjaehyung";
 
 	//printf("%s 의 길이 = %d \n", str, strlen(str));// 결과값 = 김근수 의 길이 = 6
 	//printf("값 : %s\n", str);// 결과값 = 김근수
 	//printf("배열 크기 : %d\n\n", sizeof(str));// 결과값 = 배열 크기 : 7
 	//한글은 2Byte씩 사용
 
-	for (int i = 0; i < sizeof(str)-1; i=i+2) {
+	//for (int i = 0; i < sizeof(str) - 1; i = i + 2) {
 		//printf("%c%c\n", str[i], str[i+1]);
 		/*결과값 =
 		김
 		근
 		수
 		*/
+		//}
+	char students[4][20] = {
+		"kimgeunsoo", "parkjongin", "mageunwon", "parkjaehyung"
+	};
+
+
+	char i, j;
+	for (i = 0; i < 4; i++) {
+		for (j = 0; j < 20; j++) {
+			int a; 
+			a = rand() % 4;
+			printf("%s\n", students[a]);
+			//system("cls");
+			return 0;
+		}
 	}
-	char students[8][1];
+}
+			//char input[20];
+	//scanf_s("%s", input);
 	/*
+	char students[8][10];
+	}
+	* 2차원 배열에 scanf_s로 입력해보려고 했으나 작성법 문제로 생각됨
+	* printf("1팀 이름 입력\n");
+	scanf_s("%s", &students[0][8], sizeof(students[0][8]));
+	scanf_s("%s", &students[1][8], sizeof(students[1][8]));
+	scanf_s("%s", &students[2][8], sizeof(students[2][8]));
+	scanf_s("%s", &students[3][8], sizeof(students[3][8]));
+	scanf_s("%s", &students[4][8], sizeof(students[4][8]));
+
+	printf("2팀 이름 입력\n");
+	scanf_s("%s", &students[6][8], sizeof(students[6][8]));
+	scanf_s("%s", &students[7][8], sizeof(students[7][8]));
+	scanf_s("%s", &students[8][8], sizeof(students[8][8]));
+	scanf_s("%s", &students[9][8], sizeof(students[9][8]));
+	scanf_s("%s", &students[10][8], sizeof(students[10][8]));
+
+	printf("%s%s", students[0][8]);
+	* 
+	* 
+	* 
+	* 
 		배열안에 이름을 입력하려고 시도
 	int arr[2];
 	scanf_s("%s%d", &arr[0]);
@@ -39,19 +82,6 @@ int main() {
 	char name9[8];
 	char name10[8];
 
-	printf("1팀 이름 입력\n");
-	scanf_s("%s", name1, sizeof(name1));
-	scanf_s("%s", name2, sizeof(name2));
-	scanf_s("%s", name3, sizeof(name3));
-	scanf_s("%s", name4, sizeof(name4));
-	scanf_s("%s", name5, sizeof(name5));
-
-	printf("2팀 이름 입력\n");
-	scanf_s("%s", name6, sizeof(name6));
-	scanf_s("%s", name7, sizeof(name7));
-	scanf_s("%s", name8, sizeof(name8));
-	scanf_s("%s", name9, sizeof(name9));
-	scanf_s("%s", name10, sizeof(name10));
 	
 	printf("1팀 팀장 %s\t", name1);
 	printf("%s\t", name2);
@@ -64,8 +94,7 @@ int main() {
 	printf("%s\t", name8);
 	printf("%s\t", name9);
 	printf("%s", name10);
-	return 0;
-}
+
 1팀 이름 입력
 김근수
 마근원
